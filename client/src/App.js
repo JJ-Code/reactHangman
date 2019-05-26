393073812384abce3b1a9d21a41da38ac9f2cfa5
Jaymee Liu <{jaymeeliu@gmail.com}>
2019-05-25 18:13:53 -0700

    add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all-body">
+      <div className="landing--container">
+        <div className="landing--title">
+          <h1>Movie Scrabble Hangman!</h1>
+          <p>Click a letter</p><br />
+
+
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css"
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer--bottom">
+        <p className="footer--text">©copyright JJCode | All rights reserved | &amp; development by <a href="https://github.com/JJ-Code"
+          // eslint-disable-next-line react/jsx-no-target-blank
+          target="_blank"> JJCode</a></p>
+      </div>
+    </footer>
+  )}
+
+
+
+export default Footer;
